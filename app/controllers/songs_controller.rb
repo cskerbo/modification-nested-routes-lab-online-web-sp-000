896d278b7929fbaf5cff1e83b86bd7e163f2c72e
Chris Kerbo <chris.kerbo@outlook.com>
2020-02-04 22:47:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
 
     if @song.save
-      redirect_to artists_path(@song)
+      redirect_to @song
     else
       render :new
     end
